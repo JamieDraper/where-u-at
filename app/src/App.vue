3f9afe64c6ec622b26f064cf868f7866013eb4fd
Jamie Draper <jamiedrpr@gmail.com>
2018-06-09 11:21:09 +0100

    set up WeeklyTable component

---
+++ app/src/App.vue
@@ -0,0 +1,214 @@
+<template>
+  <div id="app">
+    <WeeklyTable :entries="entries"/>
+
+    <form v-on:submit.prevent="submitEntry()">
+      
+      <div>
+        <input placeholder="Name" v-model="entry.name" type="text"/>
+      </div>
+
+      <select v-model="entry.day">
+        <option value="" disabled selected>On...</option>
+        <option value="monday">Monday</option>
+        <option value="tuesday">Tuesday</option>
+        <option value="wednesday">Wednesday</option>
+        <option value="thursday">Thursday</option>
+        <option value="friday">Friday</option>
+      </select>
+
+      <select v-model="entry.location">
+        <option value="" disabled selected>I'm working from...</option>
+        <option value="home">Home</option>
+        <option value="office" >Office</option>
+      </select>
+
+      <div>
+        <textarea placeholder="Notes" v-model="entry.notes"></textarea>
+      </div>
+
+      <button type="submit">Submit</button>
+
+    </form>
+
+  </div>
+</template>
+
+<script>
+import WeeklyTable from './components/WeeklyTable.vue'
+
+export default {
+  name: 'app',
+  components: {
+    WeeklyTable
+  },
+  data: function() {
+    return {
+      entries: {
+        monday: [
+          {
+            'name': 'monAlex',
+            'location': 'home',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'monJoe',
+            'location': 'office',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'monAnna',
+            'location': 'home',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'monJamie',
+            'location': 'office',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'monSarah',
+            'location': 'mixed',
+            'notes': 'Foo baa'
+          }
+        ],
+        tuesday: [
+          {
+            'name': 'tueAlex',
+            'location': 'home',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'tueJoe',
+            'location': 'office',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'tueAnna',
+            'location': 'home',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'tueJamie',
+            'location': 'office',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'tueSarah',
+            'location': 'mixed',
+            'notes': 'Foo baa'
+          }
+        ],
+        wednesday: [
+          {
+            'name': 'wedAlex',
+            'location': 'home',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'wedJoe',
+            'location': 'office',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'wedAnna',
+            'location': 'home',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'wedJamie',
+            'location': 'office',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'wedSarah',
+            'location': 'mixed',
+            'notes': 'Foo baa'
+          }
+        ],
+        thursday: [
+          {
+            'name': 'thurAlex',
+            'location': 'home',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'thurJoe',
+            'location': 'office',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'thurAnna',
+            'location': 'home',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'thurJamie',
+            'location': 'office',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'thurSarah',
+            'location': 'mixed',
+            'notes': 'Foo baa'
+          }
+        ],
+        friday: [
+          {
+            'name': 'friAlex',
+            'location': 'home',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'friJoe',
+            'location': 'office',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'friAnna',
+            'location': 'home',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'friJamie',
+            'location': 'office',
+            'notes': 'Foo baa'
+          },
+          {
+            'name': 'friSarah',
+            'location': 'mixed',
+            'notes': 'Foo baa'
+          }
+        ]
+      },
+      entry: {
+        name: "",
+        location: "",
+        notes:""
+      },
+      submitEntry: function() {
+        console.log('inside submitEntry');
+        console.log(this.entry);
+
+      }
+    }
+  },
+  created: function() {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+label {
+  display: block;
+}
+</style>
---
+++ app/src/components/WeeklyTable.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="Rtable Rtable--5cols Rtable--collapse js-RtableAccordions">
+  
+    <button class="Accordion" role="tab" aria-selected="true">Monday</button>
+    <div style="order:0;" class="Rtable-cell Rtable-cell--head day"><h3>Monday</h3></div>
+    <div v-for="(person, index) in entries.monday" v-bind:style="{ order: index + 1 }" class="Rtable-cell">
+    {{ person.name }}
+    </div>
+
+    
+    <button class="Accordion" role="tab" aria-selected="true">Tuesday</button>
+    <div style="order:0;" class="Rtable-cell Rtable-cell--head day"><h3>Tuesday</h3></div>
+     <div v-for="(person, index) in entries.tuesday" v-bind:style="{ order: index + 1  }" class="Rtable-cell">
+   {{ person.name }}
+      
+    </div>
+
+    
+    <button class="Accordion" role="tab" aria-selected="false">Wednesday</button>
+    <div style="order:0;" class="Rtable-cell Rtable-cell--head day"><h3>Wednesday</h3></div>
+   <div v-for="(person, index) in entries.wednesday" v-bind:style="{ order: index + 1  }" class="Rtable-cell">
+   {{ person.name }}
+      
+    </div>
+    
+    <button class="Accordion" role="tab" aria-selected="false">Thursday</button>
+      <div style="order:0;" class="Rtable-cell Rtable-cell--head day"><h3>Thursday</h3></div>
+   <div v-for="(person, index) in entries.thursday" v-bind:style="{ order: index + 1  }" class="Rtable-cell">
+   {{ person.name }}
+      
+    </div>
+    
+    <button class="Accordion" role="tab" aria-selected="false">Friday</button>
+    <div style="order:0;" class="Rtable-cell Rtable-cell--head day"><h3>Friday</h3></div>
+     <div v-for="(person, index) in entries.thursday" v-bind:style="{ order: index + 1  }" class="Rtable-cell">
+   {{ person.name }}
+      
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeeklyTable',
+  props: {
+    entries: Object
+  },
+  created: function() {
+    console.log(this.entries);
+  },
+  data: function(){
+    return {
+      styleObject: {
+        'order': 0
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ li {
+  display: block;
+ }
+
+.Rtable {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 0 3em 0;
+  padding: 0;
+}
+.Rtable-cell {
+  box-sizing: border-box;
+  flex-grow: 1;
+  width: 100%;
+  padding: 0.8em 1.2em;
+  overflow: hidden;
+  list-style: none;
+  border: solid 3px white;
+  background: rgba(112, 128, 144, 0.2);
+}
+.Rtable-cell > h1,
+.Rtable-cell > h2,
+.Rtable-cell > h3,
+.Rtable-cell > h4,
+.Rtable-cell > h5,
+.Rtable-cell > h6 {
+  margin: 0;
+}
+/* Table column sizing
+================================== */
+.Rtable--2cols > .Rtable-cell {
+  width: 50%;
+}
+.Rtable--3cols > .Rtable-cell {
+  width: 33.33%;
+}
+.Rtable--4cols > .Rtable-cell {
+  width: 25%;
+}
+.Rtable--5cols > .Rtable-cell {
+  width: 20%;
+}
+.Rtable--6cols > .Rtable-cell {
+  width: 16.6%;
+}
+
+@media all and (max-width: 500px) {
+  .hiddenSmall {
+    display: none;
+  }
+  .Rtable-cell--head.day {
+    display: none;
+  }
+}
+@media all and (min-width: 500px) {
+  .Accordion {
+    display: none;
+  }
+}
+
+</style>
